a470e2a1872b777c8b1c2a8a84eef32953891bbd
llzyy <1248295376@qq.com>
2022-07-07 22:36:04 +0800

    导入导出完成，login.vue页面编写完成

--- vue/src/views/User.vue
+++ vue/src/views/User.vue
@@ -21,8 +21,15 @@
       >
         <el-button type="danger" slot="reference" class="ml-5">批量删除 <i class="el-icon-remove-outline"></i></el-button>
       </el-popconfirm>
-      <el-button type="primary" class="ml-5">导入 <i class="el-icon-bottom"></i></el-button>
-      <el-button type="primary">导出 <i class="el-icon-top"></i></el-button>
+      <el-upload
+          action="http://localhost:8080/user/import" style="display: inline-block" class="mr-5"
+          :show-file-list="false"
+          :accept="'.xlsx'"
+          :on-success="handleExcelImportSuccess">
+        <el-button type="primary" class="ml-5">导入 <i class="el-icon-bottom"></i></el-button>
+      </el-upload>
+
+      <el-button type="primary" @click="exp">导出 <i class="el-icon-top"></i></el-button>
     </div>
 
     <el-table :data="tableData" border stripe :header-cell-class-name="headerBg" @selection-change="handleSelectionChange">
@@ -99,7 +106,7 @@ export default {
       tableData: [],
       total: 0,
       pageNum: 1,
-      pageSize: 5,
+      pageSize: 10,
       username: "",
       email: "",
       address: "",
@@ -188,6 +195,13 @@ export default {
     handleCurrentChange(pageNum){
       console.log(pageNum)
       this.pageNum = pageNum
+      this.load()
+    },
+    exp(){
+      window.open("http://localhost:8080/user/export")
+    },
+    handleExcelImportSuccess(){
+      this.$message.success("文件导入成功！")
       this.load()
     }
   }
